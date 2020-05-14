ee85a2ce69509e5c2759c52dd2bbba5f00c66a44
MalkamGolden <45684143+MalkamGolden@users.noreply.github.com>
2020-05-14 11:41:20 -0700

    Add files via upload

---
+++ Server Lift iOS App/UpButton.swift
@@ -0,0 +1,31 @@
+//
+//  UpButton.swift
+//  642project
+//
+//  Created by Malkam on 4/5/20.
+//  Copyright © 2020 Malkam. All rights reserved.
+//
+/*
+import SwiftUI
+
+struct Upbutton: View>: View {
+    
+    let action: () -> Void
+    let content: WhateverYouWant
+    
+    init(action:() @escaping -> Void, @ViewBilder content: () -> WhateverYouWant) {
+        self.action = action
+        self.content = content()
+    }
+    
+    var body: some View {
+        Button(action: action) {
+            content
+                .padding()
+                .frame(minWidth:0, maxWidth: .infinity)
+                .background(Capsule().fil(Color.blue))
+                .foregroundColor(.white);
+        }
+    }
+}
+*/
